8bc8e9e8c887259da34b93b7e8ed18197821dca2
Himanshu Singh <himanshusinghchauhan@192.168.158.19>
2024-12-28 17:29:20 +0530

    website done

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+          At Madhur Dairy, we are committed to providing the highest quality dairy products and exceptional customer service. Whether you're a retail partner, a business, or a consumer, we ensure that you receive the finest, freshest products with the utmost care and efficiency.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
